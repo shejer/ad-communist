093d3fa4b4612f87c7261f3f456c6586e920ad7f
hejer <hejer.shaiek@gmail.com>
2020-03-19 15:59:31 +0100

    Updates

---
+++ precache-manifest.8d4685f899ab870f2c29af0d2cc59f61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f20347741c6d48b5f2564b355f2b174",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ea19a3cf2e89a108007",
+    "url": "/static/css/main.ecc5a1f9.chunk.css"
+  },
+  {
+    "revision": "63e65e49fce72c66688f",
+    "url": "/static/js/2.3afa5b34.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.3afa5b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea19a3cf2e89a108007",
+    "url": "/static/js/main.a59ee428.chunk.js"
+  },
+  {
+    "revision": "dba008b51f31036cb213",
+    "url": "/static/js/runtime-main.cd0d3613.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
