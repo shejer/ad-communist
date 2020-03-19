4a5dcf0a7d596708b43f7cd0b2b6b04a70ff598a
hejer <hejer.shaiek@gmail.com>
2020-03-19 16:02:04 +0100

    Updates

---
+++ precache-manifest.c67e8d2815174ca140f8ed44e9b6a119.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6559ae107b9f4ce58748c4eaf632a8",
+    "url": "/ad-communist/index.html"
+  },
+  {
+    "revision": "d7e7c19bf81f77d70694",
+    "url": "/ad-communist/static/css/main.ecc5a1f9.chunk.css"
+  },
+  {
+    "revision": "63e65e49fce72c66688f",
+    "url": "/ad-communist/static/js/2.3afa5b34.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ad-communist/static/js/2.3afa5b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7e7c19bf81f77d70694",
+    "url": "/ad-communist/static/js/main.5b028d97.chunk.js"
+  },
+  {
+    "revision": "43a7571c6105b1496663",
+    "url": "/ad-communist/static/js/runtime-main.68588b3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ad-communist/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d4685f899ab870f2c29af0d2cc59f61.js"
+  "/ad-communist/precache-manifest.c67e8d2815174ca140f8ed44e9b6a119.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ad-communist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
